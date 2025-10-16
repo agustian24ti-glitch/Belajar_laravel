d93b2349dbde7083ccc4fc8c5bad6e4c172920ce
agustian24ti <agustian24ti@mahasiswa.pcr.ac.id>
2025-10-16 11:28:28 +0700

    p6

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PelangganController;
 
 
 
@@ -38,3 +40,7 @@ Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::resource('pelanggan', PelangganController::class);
